32a76c689ab47382bdd4cf260fc2e8186b65e7fe
NFerron24 <67276148+NFerron24@users.noreply.github.com>
2020-07-20 17:40:04 +0200

    Add files via upload

---
+++ kroeg.js
@@ -0,0 +1,18 @@
+const age = 30;
+if (age >= 18) {
+  console.log("Je bent 18 jaar of ouder");
+} else if (age < 18) {
+  console.log("je bent nog niet oud genoeg om naar binnen te gaan");
+}
+const isFemale = false;
+if (isFemale) {
+  console.log("Je bent een vrouw, welkom op ladiesnight!");
+} else {
+  console.log("je bent geen vrouw, je mag helaas geen kaartje kopen");
+}
+const DriverStatus = "Bobx";
+if (DriverStatus == "Bob") {
+  console.log("Jij bent de Bob, je mag naar huis rijden");
+} else {
+  console.log("Je bent niet de Bob, je mag niet achter het stuur");
+}
